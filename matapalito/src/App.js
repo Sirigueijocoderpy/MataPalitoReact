71b54fac400242d835b266338bec0e5155cd6b82
joão <jgablopes2008@gmail.com>
2025-05-05 13:38:54 -0300

    inicio da logica

--- matapalito/src/App.js
+++ matapalito/src/App.js
@@ -4,45 +4,71 @@ let palavra = "matapalito";
 let vidas = 6
 let arrayletras = []
 let cemiterio = []
+
+for (let i = 0; i < palavra.length; i++){
+  arrayletras[i] = ''
+
+}
+
 function Tentar() {
-  document.getElementById('letter').value = document.getElementById('letter').value.toLowerCase()
-  arrayletras.push(document.getElementById('letter').value)}
-  //Game Loop
-  while(arrayletras !== palavra || vidas > 0){
-    if (arrayletras.includes(arrayletras) === false){
+
+  let letra = document.getElementById('letter').value.toLowerCase()
+
+  if (palavra.indexOf(letra) != -1 || palavra.indexOf(letra) != ''){
+
+    while (palavra.indexOf(letra) != -1){
+      let indice = palavra.indexOf(letra)
+
+      arrayletras[indice] = letra
+
+      palavra = palavra.replace(letra, ' ')
       
-      vidas = vidas - 1
     }
-    else if (arrayletras.includes(arrayletras) === true){
-      console.log("Você acertou a letra")
-    }
+
+  } else {
+
+    vidas -= 1
+    cemiterio.push(letra)
+
+    document.getElementById('vidas').innerHTML = `Número de tentativas: ${vidas}`
+    document.getElementById('cemiterio').innerHTML = `Letras já testadas: ${cemiterio}`
 
   }
-  
-    
+
+}
+
+
+
 function App() {
   return (
-    
+
     <div className='tentativas'>
+
       <h1>Jogo da Forca</h1>
-      <input type='text' id='letter' maxLength='1'placeholder ='Digite uma letra' className='imput-letter'></input>
+
+      <input type='text' id='letter' maxLength='1' placeholder='Digite uma letra' className='imput-letter'></input>
+
       <br></br>
-      <button onClick='Tentar()'>Advinhar</button>
-      <p>
-        Numero de tentantativas: {vidas}
+
+      <button onClick={Tentar}>Advinhar</button>
+
+      <p id='vidas'>
+        Número de tentativas: {vidas}
 
       </p>
+
       <p>
-        Numero de letras: {palavra.length}
+        Número de letras: {palavra.length}
       </p>
-      <p>
-        Letras já tentadas: {arrayletras}
+
+      <p id='cemiterio'>
+        Letras já testadas:
       </p>
+
     </div>
-    
-    
+
+
   )
 }
-console.log(arrayletras.includes(arrayletras))
 
 export default App;
