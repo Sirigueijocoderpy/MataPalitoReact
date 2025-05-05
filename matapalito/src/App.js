f84687e59c679cda02c4314293de76ceb130443d
Hector <hmbertoco@gmail.com>
2025-05-05 15:44:04 -0300

    Comite

---
+++ matapalito/src/App.js
@@ -0,0 +1,130 @@
+
+import './App.css';
+
+let palavra = "matapalito";
+let palavra_arr = palavra
+let vitoria = false
+let vidas = 6
+let arrayletras = []
+let cemiterio = []
+
+for (let i = 0; i < palavra.length; i++) {
+  arrayletras[i] = ' '
+
+}
+
+function Tentar() {
+
+  let letra = document.getElementById('letter').value.toLowerCase()
+
+  if (vidas > 0) {
+
+    if (letra == '') {
+      return
+
+    }
+
+    if (arrayletras.indexOf(letra) != -1 || cemiterio.indexOf(letra) != -1) {
+      return
+
+    }
+
+    if (palavra.indexOf(letra) != -1) {
+
+      while (palavra.indexOf(letra) != -1) {
+        let indice = palavra.indexOf(letra)
+
+        arrayletras[indice] = letra
+
+        palavra = palavra.replace(letra, ' ')
+
+      }
+
+      document.getElementById('resposta').innerHTML = 'Resposta: '
+
+      for (let i = 0; i < arrayletras.length; i++) {
+        document.getElementById('resposta').innerHTML += arrayletras[i]
+
+      }
+
+    } else {
+
+      vidas -= 1
+      cemiterio.push(letra)
+
+      document.getElementById('vidas').innerHTML = `Número de tentativas: ${vidas}`
+      document.getElementById('cemiterio').innerHTML = `Letras já testadas: ${cemiterio}`
+
+    }
+
+    Vitoria()
+
+    if (vitoria == true) {
+      alert('Você venceu! \n' + 'A palavra era: ' + palavra_arr)
+      window.location.reload()
+
+    }
+
+  } else {
+
+    alert('Suas vidas acabaram!')
+
+  }
+
+}
+
+
+function Vitoria() {
+
+  for (let i = 0; i < palavra_arr.length; i++) {
+    if (arrayletras[i] == palavra_arr[i]) {
+      vitoria = true
+
+    } else {
+      vitoria = false
+      break
+    }
+
+  }
+
+}
+
+
+
+function App() {
+  return (
+
+    <div className='tentativas'>
+
+      <h1>Jogo da Forca</h1>
+
+      <input type='text' id='letter' maxLength='1' placeholder='Digite uma letra' className='imput-letter'></input>
+
+      <br></br>
+
+      <button onClick={Tentar}>Advinhar</button>
+
+      <p id='resposta'>
+        Resposta: 
+      </p>
+
+      <p id='vidas'>
+        Número de tentativas: {vidas}
+
+      </p>
+
+      <p>
+        Número de letras: {palavra.length}
+      </p>
+
+      <p id='cemiterio'>
+        Letras já testadas:
+      </p>
+
+    </div>
+
+
+  )
+}
+
+export default App;
