7424c86df6c5573a444c40d1cec50750b9b7bbc1
heitor <hmbertoco@gmail.com>
2025-05-05 09:24:50 -0300

    app Changes

--- matapalito/src/App.js
+++ matapalito/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+let palavra = "matapalito";
+let vidas = 6
+let arrayletras = []
+function tentar() {
+  document.getElementById('letter').value = document.getElementById('letter').value.toLowerCase()
+  let arrayletras += document
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div class='tentativas'>
+      <h1>Jogo da Forca</h1>
+      <input type='text' id='letter' maxlength='1'placeholder ='Digite uma letra' class='imput-letter'></input>
+      <br></br>
+      <button onclick='tentar()'>Advinhar</button>
+      <p>
+        Numero de tentantativas: {vidas}
+
+      </p>
+      <p>
+        Numero de letras: {palavra.length}
+      </p>
     </div>
-  );
+    
+    
+  )
 }
+
 
 export default App;
